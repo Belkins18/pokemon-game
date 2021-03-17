7bb48a8e564afe0baade0a56a76db73a3af3ee6c
belkins <belibov.nikolay@gmail.com>
2021-03-18 01:39:57 +0200

    task 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,8 @@ const Header = ({
   return (
     <header className={s.root}>
       <div className={s.forest}></div>
+      <div className={s.silhouette}></div>
+      <div className={s.moon}></div>
       <div className={s.container}>
         <h1>{title}</h1>
         <p>{desk}</p>

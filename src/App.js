c5b1337663c8a3f825293017310953e60c29c2b1
Nikolay Belibov <nikolay.belibov@dstarlab.com>
2021-03-19 10:43:16 +0200

    add firebase to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useLocation, Route, Switch, Redirect } from "react-router-dom";
+import {useLocation, Route, Switch, Redirect} from "react-router-dom";
+import firebase from "firebase";
 import cn from "classnames";
 // Components
 import AboutPage from "./routes/AboutPage";
@@ -11,37 +12,55 @@ import Footer from "./components/Footer";
 //Styles
 import s from "./app.module.css";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCTlVPM8uKX0ZltL0pnFA4j4W_iUsarYLc",
+    authDomain: "pokemon-game-8bea6.firebaseapp.com",
+    databaseURL: "https://pokemon-game-8bea6-default-rtdb.firebaseio.com",
+    projectId: "pokemon-game-8bea6",
+    storageBucket: "pokemon-game-8bea6.appspot.com",
+    messagingSenderId: "862886558646",
+    appId: "1:862886558646:web:4abfbcf84302bc8e829f17"
+}
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+database.ref('pokemons').once('value', (snapshot) => {
+    console.log(snapshot.val());
+})
+
+
 const App = () => {
-  const location = useLocation();
-  const noMatch = () => (location.pathname === "/" || location.pathname === "/welcome")
+    const location = useLocation();
+    const noMatch = () => (location.pathname === "/" || location.pathname === "/welcome")
 
-  return (
-    <Switch>
-      <Route path="/404" component={NotFound} />
+    return (
+        <Switch>
+            <Route path="/404" component={NotFound}/>
 
-      <Route>
-        <>
-          <MenuHeader bgActive={!noMatch()} />
-          <div
-            className={cn(s.wrap, {
-              [s.isHomePage]: noMatch(),
-            })}
-          >
-            <Switch>
-              <Route path="/" exact component={HomePage} />
-              <Route path="/welcome" component={HomePage} />
-              <Route path="/game" component={GamePage} />
-              <Route path="/about" component={AboutPage} />
-              <Route path="/contact" component={ContactPage} />
-              <Route render={() => <Redirect to="/404"/>} />
-            </Switch>
-          </div>
-          <Footer />
-        </>
-      </Route>
+            <Route>
+                <>
+                    <MenuHeader bgActive={!noMatch()}/>
+                    <div
+                        className={cn(s.wrap, {
+                            [s.isHomePage]: noMatch(),
+                        })}
+                    >
+                        <Switch>
+                            <Route path="/" exact component={HomePage}/>
+                            <Route path="/welcome" component={HomePage}/>
+                            <Route path="/game" component={GamePage}/>
+                            <Route path="/about" component={AboutPage}/>
+                            <Route path="/contact" component={ContactPage}/>
+                            <Route render={() => <Redirect to="/404"/>}/>
+                        </Switch>
+                    </div>
+                    <Footer/>
+                </>
+            </Route>
 
-    </Switch>
-  );
+        </Switch>
+    );
 };
 
 export default App;

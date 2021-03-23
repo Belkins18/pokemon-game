c626373a3e32b120250595bc55a879bcaa493739
Nikolay Belibov <nikolay.belibov@dstarlab.com>
2021-03-23 17:49:44 +0200

    selectedPokemons from useState

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,16 +1,42 @@
+import {useState} from "react";
 import {Route, Switch, useRouteMatch} from "react-router-dom";
 import StartPage from "./routes/Start";
 import BoardPage from "./routes/Board";
 import FinishPage from "./routes/Finish";
+import {PokemonContext} from "../../context/pokemonContext";
 
 const GamePage = () => {
+    const [selectedPokemons, setSelectedPokemons] = useState({});
+    console.log("£££: selectedPokemons: ", selectedPokemons);
+
     const match = useRouteMatch();
+
+    const handleSelectedPokemons = (uuid, pokemon) => {
+        setSelectedPokemons(prevState => {
+            if (prevState[uuid]) {
+                const copyState = {...prevState};
+                delete copyState[uuid];
+
+                return copyState;
+            }
+            return {
+                ...prevState,
+                [uuid]: pokemon
+            }
+        })
+    }
     return (
-        <Switch>
-            <Route path={`${match.path}/`} exact component={StartPage} />
-            <Route path={`${match.path}/board`} component={BoardPage} />
-            <Route path={`${match.path}/finish`} component={FinishPage} />
-        </Switch>
+        <PokemonContext.Provider value={{
+            pokemon: selectedPokemons,
+            onSelectedPokemons: handleSelectedPokemons
+        }}>
+            <Switch>
+                <Route path={`${match.path}/`} exact component={StartPage}/>
+                <Route path={`${match.path}/board`} component={BoardPage}/>
+                <Route path={`${match.path}/finish`} component={FinishPage}/>
+            </Switch>
+        </PokemonContext.Provider>
+
     );
 };
 export default GamePage;
--- src/routes/GamePage/routes/Start/index.js
+++ src/routes/GamePage/routes/Start/index.js
@@ -3,23 +3,29 @@ import {useState, useEffect, useContext} from "react";
 import PokemonCard from "../../../../components/PokemonCard";
 // Context
 import {FirebaseContext} from "../../../../context/firebaseContext";
+import {PokemonContext} from "../../../../context/pokemonContext";
 // Styles
 import s from "./style.module.css";
 
 const StartPage = () => {
-    const firebase = useContext(FirebaseContext);
+    const firebaseContext = useContext(FirebaseContext);
+    const pokemonsContext = useContext(PokemonContext);
+
     const [pokemons, setPokemons] = useState({});
 
     useEffect(() => {
-        firebase.getPokemonsSocket((pokemons) => {
+        firebaseContext.getPokemonsSocket((pokemons) => {
             setPokemons(pokemons);
         });
-        return () => firebase.offPokemonsSocket()
+        return () => firebaseContext.offPokemonsSocket()
     },[]);
 
     const handleChangeActiveSelected = (uuid, isSelected) => {
-        console.log(uuid);
+        const pokemon = {...pokemons[uuid]}
+        pokemonsContext.onSelectedPokemons(uuid, pokemon);
+
         if (pokemons[uuid]) {
+
             const copyState = {...pokemons};
             copyState[uuid]["isSelected"] = !isSelected
 
